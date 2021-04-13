fbfa846c86ef783ea5ff000b34b69fd53947501a
greenloper <77944828+greenloper@users.noreply.github.com>
2021-04-13 23:46:33 +0900

    Add files via upload

---
+++ 큰 수 만들기.py
@@ -0,0 +1,13 @@
+number="1924"
+k=2
+
+stack=[number[0]]
+for num in number[1:]:
+    while len(stack)>0 and stack[-1]<num and k>0:
+        k-=1
+        stack.pop()
+    stack.append(num)
+
+if k!=0:
+    stack=stack[:-k]
+print(''.join(stack))
