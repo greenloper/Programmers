f899e2c11cc97a2dc19f8f3a0b104d9e12d7c648
greenloper <77944828+greenloper@users.noreply.github.com>
2021-05-02 10:55:35 +0900

    Add files via upload

---
+++ 후보키.py
@@ -0,0 +1,24 @@
+from itertools import combinations as combi
+
+relation=[["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]
+
+row=len(relation)
+col=len(relation[0])
+
+candidates=[]
+for i in range(1, col+1):
+    candidates.extend(combi(range(col),i))
+    
+unique=[]
+for candi in candidates:
+    tmp=[tuple([item[i] for i in candi]) for item in relation]
+    if len(set(tmp))==row:
+        unique.append(candi)
+
+answer=set(unique)
+for i in range(len(unique)):
+    for j in range(i+1, len(unique)):
+        if len(unique[i])==len(set(unique[i])&set(unique[j])):
+            answer.discard(unique[j])
+
+print(len(answer))
