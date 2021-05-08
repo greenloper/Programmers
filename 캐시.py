edcb443b0df8df5775e4b2835fe6099dcc0aafae
greenloper <77944828+greenloper@users.noreply.github.com>
2021-05-08 23:47:24 +0900

    Add files via upload

---
+++ 캐시.py
@@ -0,0 +1,21 @@
+cacheSize=3
+cities=["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]
+time = 0
+cache = []
+cities = [city.lower() for city in cities]
+if cacheSize != 0:
+    for city in cities:
+        if city in cache:
+            cache.pop(cache.index(city))
+            cache.append(city)
+            time += 1
+        else:
+            if len(cache) < cacheSize:
+                cache.append(city)
+                time += 5
+            else:
+                cache.pop(0)
+                cache.append(city)
+                time += 5
+else: time += len(cities) * 5
+print(time)
